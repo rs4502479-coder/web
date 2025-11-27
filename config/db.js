51e48278295de20bc52bf7eda41702eec72008e3
rs4502479-coder <rs4502479@gmail.com>
2025-11-27 03:46:09 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,29 +3,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST || "127.0.0.1",
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "",
-  database: process.env.DATABASE_NAME || "bdgusdtmining",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 module.exports = pool;
-
-
-// import mysql from "mysql2/promise";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const db = mysql.createPool({
-//   host: process.env.DATABASE_HOST,
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-//   port: 3306,
-//   connectionLimit: 10,
-// });
-
-// export default db;
