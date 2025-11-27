9d720523c340eef11982a01697fb67fb6967c1ee
rs4502479-coder <rs4502479@gmail.com>
2025-11-27 04:09:59 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-// config/db.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
@@ -7,10 +6,8 @@ const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: process.env.MYSQLPORT,
+  ssl: { rejectUnauthorized: false } 
 });
 
 module.exports = pool;
