7a000205e096a020ce6168c997a6996456613207
rs4502479-coder <rs4502479@gmail.com>
2025-12-12 09:20:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,10 @@
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => console.log("Server running on port", PORT));
 
-// server.js
+
+
+ // server.js
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -39,8 +42,6 @@ const txRoutes = require("./routes/transactions");
 const adminRoutes = require("./routes/admin");
 const adminAuthRoutes = require("./routes/admin_auth");
 const userRoutes = require("./routes/user");
-
-// 🔥 ADD THIS
 const withdrawRoutes = require("./routes/withdraw");
 
 const app = express();
@@ -48,15 +49,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 
-// API
 app.use("/api/auth", authRoutes);
 app.use("/api/admin/auth", adminAuthRoutes);
 app.use("/api/tasks", tasksRoutes);
 app.use("/api/transactions", txRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
-
-// 🔥 ADD THIS
 app.use("/api/withdraw", withdrawRoutes);
 
 const PORT = process.env.PORT || 3000;
