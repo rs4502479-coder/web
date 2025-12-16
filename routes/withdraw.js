815b4228cd7bf2fedc860a5179d6b69d3d0a94c2
rs4502479-coder <rs4502479@gmail.com>
2025-12-16 05:52:35 -0800

    Update withdraw.js

--- routes/withdraw.js
+++ routes/withdraw.js
@@ -140,7 +140,7 @@
 
 const express = require("express");
 const db = require("../config/db");
-const auth = require("../middleware/auth");
+const {auth} = require("../middleware/auth");
 const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
@@ -200,6 +200,7 @@ router.post("/request", auth, async (req, res) => {
 });
 
 module.exports = router;
+
 
 
 
